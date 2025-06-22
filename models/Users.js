8109dbb90ec1d060940d557ab0ce35b216b4d814
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-06-22 19:54:58 +0300

    craete Users modele

---
+++ models/Users.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const usersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+});
+
+const User = mongoose.model("User", usersSchema);
+
+module.exports = User;
