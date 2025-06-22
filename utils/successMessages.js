d14ec9c06971fc76ffbfc89a2f423dc671d7a9ae
Ibrahim-Karagaji <ibrahimKaragaji@gmail.com>
2025-06-22 19:37:09 +0300

    create errorMessages.js file to Unify the success meassges

---
+++ utils/successMessages.js
@@ -0,0 +1,13 @@
+const successMessages = {
+  auth: {
+    registered: "Account created successfully",
+    loggedIn: "Logged in successfully",
+  },
+  user: {
+    created: "user added successfully",
+    updated: "user updated successfully",
+    deleted: "user deleted successfully",
+  },
+};
+
+module.exports = successMessages;
